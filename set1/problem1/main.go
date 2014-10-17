681ff0a4d23e298fd3b8d0aec8dd4b9e41af562b
Petros Angelatos <petrosagg@gmail.com>
2014-10-17 23:28:33 +0100

    Fix base64 encoding

--- set1/problem1/main.go
+++ set1/problem1/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"encoding/hex"
 	"flag"
 	"fmt"
 )
@@ -24,22 +23,22 @@ func getB64Char(c byte) byte {
 }
 
 func b64Encode(src []byte) string {
-	var ret = ""
+	var buf = make([]byte, (len(src) + 2) / 3 * 4)
+
 	for i := 0; i < len(src)/3; i++ {
 		c1 := (src[i*3] & 0xfc) >> 2
 		c2 := ((src[i*3] & 0x03) << 4) + ((src[i*3+1] & 0xf0) >> 4)
 		c3 := ((src[i*3+1] & 0x0f) << 2) + ((src[i*3+2] & 0xc0) >> 6)
 		c4 := src[i*3+2] & 0x3f
 
-		ret += c1
-		fmt.Printf("%c", getB64Char(c1))
-		fmt.Printf("%c", getB64Char(c2))
-		fmt.Printf("%c", getB64Char(c3))
-		fmt.Printf("%c", getB64Char(c4))
+		buf[i * 4 + 0] = getB64Char(c1)
+		buf[i * 4 + 1] = getB64Char(c2)
+		buf[i * 4 + 2] = getB64Char(c3)
+		buf[i * 4 + 3] = getB64Char(c4)
 	}
 	fmt.Printf("\n")
 
-	return hex.Dump(src)
+	return string(buf)
 }
 
 func hexDecode(hex string) []byte {
